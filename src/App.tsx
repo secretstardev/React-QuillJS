d4335b2454454f121724239c58efb52f76bfb08e
dmytroivantsiv <dmytroivantsiv1@gmail.com>
2024-08-20 01:26:04 -0400

    update dynamic loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
-import React, { useEffect } from 'react'
 import './App.css'
 import 'quill/dist/quill.snow.css';
 
+import { useQuill } from 'react-quilljs';
+// or const { useQuill } = require('react-quilljs');
+
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+// or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
+
 function App() {
 
-  const [quill, setQuill] = React.useState<any>(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('react-quilljs').then(({ useQuill }) => {
-        const { quill } = useQuill();
-        setQuill(quill);
-
-        if (quill) {
-          quill.clipboard.dangerouslyPasteHTML('<p>Hello, Quill!</p>');
-        }
-      });
-    }
-  }, []);
+  const { quillRef } = useQuill();
 
   return (
-    <>
-      <div style={{ width: 600, height: 400 }}>
-        <div ref={(el) => el && quill && quill.setContainer(el)} />
-      </div>
-    </>
-  )
+    <div style={{ width: 500, height: 300 }}>
+      <div ref={quillRef} />
+    </div>
+  );
 }
 
 export default App
